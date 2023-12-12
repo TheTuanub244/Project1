d976228deae110f23b8c1042e44aa458056cebf3
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2023-12-12 22:50:17 +0700

    asd

--- client/src/app/components/layouts/Shipping.js
+++ client/src/app/components/layouts/Shipping.js
@@ -9,38 +9,61 @@ import {
   Stack,
 } from "@mui/material";
 import { useContext, useEffect, useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import AddressModal from "./modals/AddressModal";
 
 const { default: Navbar } = require("./Navbar");
 
 const Shipping = () => {
   const [checkout, setCheckout] = useState();
+  const [totalAddress, setTotalAddress] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedProvince, setSelectedProvince] = useState();
+  const [selecetedDistrict, setSelectedDistrict] = useState();
+  const [selectedWard, setSelectedWard] = useState();
+  const [moreAddress, setMoreAddress] = useState();
+  const [address, setAddress] = useState();
+  const [addedAddress, setAddedAddress] = useState([]);
   useEffect(() => {
     const getCheckout = JSON.parse(localStorage.getItem("checkoutInfo"));
     setCheckout(getCheckout);
   }, []);
   useEffect(() => {
-    console.log(checkout);
-  }, [checkout]);
+    setAddedAddress([...addedAddress, address]);
+  }, [address]);
+  useEffect(() => {
+    console.log(addedAddress);
+    console.log(checkout?.address?.ward?.ward_id);
+  }, [addedAddress]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <Navbar />
-      <div className="container">
+      <div className="shipping-checkout-container">
         <Stack className="stack">
           <h2>Thông tin giao hàng</h2>
           <RadioGroup
             aria-labelledby="demo-controlled-radio-buttons-group"
             name="controlled-radio-buttons-group"
             className="radio-group"
+            defaultValue={checkout?.address?.ward?.ward_id}
           >
             {checkout && (
               <FormControlLabel
                 className="address-box"
-                value={5}
+                value={checkout?.address?.ward?.ward_id}
                 control={<Radio />}
                 label={
                   <div className="address-info">
                     <h3>{checkout?.address?.moreAddress}</h3>
-                    <p>
+                    <p
+                      style={{
+                        marginTop: -20,
+                      }}
+                    >
                       {checkout?.address?.ward?.ward_name},{" "}
                       {checkout?.address?.district?.district_name},{" "}
                       {checkout?.address?.province?.province_name}
@@ -49,9 +72,81 @@ const Shipping = () => {
                 }
               />
             )}
+            {addedAddress &&
+              addedAddress?.map(
+                (index) =>
+                  index != undefined && (
+                    <FormControlLabel
+                      className="address-box"
+                      value={index?.ward?.ward_id}
+                      control={<Radio />}
+                      label={
+                        <div className="address-info">
+                          <h3>{index?.moreAddress}</h3>
+                          <p
+                            style={{
+                              marginTop: -20,
+                            }}
+                          >
+                            {index?.ward?.ward_name},{" "}
+                            {index?.district?.district_name},{" "}
+                            {index?.province?.province_name}
+                          </p>
+                        </div>
+                      }
+                    />
+                  )
+              )}
           </RadioGroup>
+          <button className="add-new" onClick={() => setOpen(true)}>
+            {" "}
+            <FaPlus /> Thêm địa chỉ khác
+          </button>
+          <div className="button-group">
+            <button className="back-btn">Quay lại</button>
+            <button className="checkout-btn">Thanh toán</button>
+          </div>
+        </Stack>
+        <Stack className="shipping-cart">
+          <h3>Sản phẩm trong giỏ hàng</h3>
+          {checkout &&
+            checkout.totalCart.map((index) => (
+              <div className="shipping-item">
+                <div className="item-image">
+                  <img src={index.data.image1} />
+                  <p className="item-amount">{index.count}</p>
+                </div>
+                <div className="shipping-item-detail">
+                  <p>{index.data.itemName}</p>
+                  <p>
+                    Total: {index.totalPrice}
+                    <sup>₫</sup>
+                  </p>
+                </div>
+              </div>
+            ))}
+          <div className="total-checkout">
+            <p>Tổng tiền: </p>
+            <p>
+              {checkout?.totalCheckOut?.totalPrice}
+              <sup>₫</sup>
+            </p>
+          </div>
         </Stack>
       </div>
+      <AddressModal
+        open={open}
+        handleClose={handleClose}
+        selecetedDistrict={selecetedDistrict}
+        selectedProvince={selectedProvince}
+        selectedWard={selectedWard}
+        setSelectedDistrict={setSelectedDistrict}
+        setSelectedProvince={setSelectedProvince}
+        setSelectedWard={setSelectedWard}
+        setMoreAddress={setMoreAddress}
+        moreAddress={moreAddress}
+        setAddress={setAddress}
+      />
     </>
   );
 };
